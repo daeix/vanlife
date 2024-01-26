5f78c9571c8281867c0f67aaaa5fc996591518c0
daeix <110390090+daeix@users.noreply.github.com>
2024-01-26 10:37:36 +0000

    update

--- pages/Host/Income.jsx
+++ pages/Host/Income.jsx
@@ -13,11 +13,7 @@ export default function Income() {
         Last <span>30 days</span>
       </p>
       <h2>$2,260</h2>
-      <img
-        className="graph"
-        src="/assets/images/income-graph.png"
-        alt="Income graph"
-      />
+      <img className="graph" src="/income-graph.png" alt="Income graph" />
       <div className="info-header">
         <h3>Your transactions (3)</h3>
         <p>
